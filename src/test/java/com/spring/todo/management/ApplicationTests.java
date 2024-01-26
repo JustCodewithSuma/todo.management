827437e504b83107c60a63c93e556c0d460f2a34
Suma Janardhan <sumajanardhan@gmail.com>
2024-01-26 22:08:15 +0100

    Initial commit

---
+++ src/test/java/com/spring/todo/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.todo.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
